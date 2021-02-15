98ffba14dd6240bec2c1732f5a98f5887a4f3540
Elio Gonzalez <elio@opiliosoft.com>
2021-02-15 15:07:12 -0500

    Remove assembly version from here since build server is versioning these now.

--- src/config/GlobalAssemblyInfo.cs
+++ src/config/GlobalAssemblyInfo.cs
@@ -4,8 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("DotNetProjects.Migrator")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: ComVisible(false)]
-#if NETSTANDARD
-[assembly: AssemblyVersion("5.0.0.1")]
-#else
-[assembly: AssemblyVersion("5.0.0.1")]
-#endif
+ 
